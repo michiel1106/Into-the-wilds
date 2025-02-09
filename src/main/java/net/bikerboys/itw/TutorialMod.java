1497d522f4ee8d192483cdecc1425dd639e36a41
michiel1106 <michielimmink@gmail.com>
2025-02-09 18:23:29 +0100

    test second commit

--- src/main/java/net/bikerboys/itw/TutorialMod.java
+++ src/main/java/net/bikerboys/itw/TutorialMod.java
@@ -20,7 +20,7 @@ public class TutorialMod
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "itw";
-    // Directly reference a slf4j logger
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod()
